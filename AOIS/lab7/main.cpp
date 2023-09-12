637fe04d582ed11651b2e8a5d36d9c5f8d5955d5
Alex Protas <92787901+fasolinger@users.noreply.github.com>
2023-09-13 01:26:29 +0300

    Update main.cpp

--- AOIS/lab7/main.cpp
+++ AOIS/lab7/main.cpp
@@ -1,62 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <cstdlib>
+#icnlude <string>
+#include <algorithm>
+#include <random>
+#include <functional>
+#include <locale>
 
-using namespace std;
+std::vector<std::vector<bool>> generate_random_table() {
+    std::vector<std::vector<bool>> table;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(0, 1);
 
-vector<vector<bool>> generate_random_table() {
-    vector<bool> row;
-    vector<vector<bool>> table;
     for (int j = 0; j < 20; j++) {
+        std::vector<bool> row;
         for (int i = 0; i < 10; i++) {
-            if (rand() % 2 == 1) 
-                row.push_back(1);
-            else 
-                row.push_back(0);
+            bool value = dist(gen);
+            row.push_back(value);
         }
         table.push_back(row);
-        row.clear();
     }
-    return table;
-}
 
-struct Trigger {
-    bool g;
-    bool l;
+        return table;
 };
 
-Trigger recursive_function(string argument, vector<bool> word, int i) {
-    Trigger final_state;
-    Trigger initial_state;
+struct Trig {
+    bool L;
+    bool G;
+};
+
+std::vector<bool> rec_func(const std::string& argument, const std::vector<bool>& word, int i) {
+    std::vector<bool> final_state(2, false);
+    std::vector<bool> initial_state(2, false);
+
     if (i + 1 == word.size()) {
-        initial_state.g = false;
-        initial_state.l = false;
+        initial_state[0] = false;
+        initial_state[1] = false;
     }
     else {
-        initial_state = recursive_function(argument, word, i + 1);
+        initial_state = rec_func(argument, word, i + 1);
     }
-    if (initial_state.g || (argument[i] == '0' && word[i] == 1 && !initial_state.l)) {
-        final_state.g = true;
+
+    if (initial_state[0] || (argument[i] == '0' && word[i] && !initial_state[1])) {
+        final_state[0] = true;
     }
-    else {
-        final_state.g = false;
+
+    if (initial_state[1] || (argument[i] == '1' && !word[i] && !initial_state[0])) {
+        final_state[1] = true;
     }
-    if (initial_state.l || (argument[i] == '1' && word[i] == 0 && !initial_state.g)) {
-        final_state.l = true;
-    }
-    else {
-        final_state.l = false;
-    }
+
     return final_state;
 };
 
 
-void print_word(const vector<bool>& word_to_print) {
-    for (size_t i = 0; i < word_to_print.size(); i++) {
-        cout << word_to_print[i];
+void print_word(vector<bool>& word) {
+    for (size_t i = 0; i < word.size(); i++) {
+        std::cout << word[i];
     }
-}
+};
 
 vector<vector<bool>> find_best_matches(const vector<vector<bool>>& book, const vector<bool>& word, int* best_match_discharge) {
     vector<vector<bool>> best_matches;
@@ -93,148 +94,147 @@ vector<vector<bool>> find_best_matches(const vector<vector<bool>>& book, const v
     }
 
     return best_matches;
-}
-
-
-void print_matches(const vector<vector<bool>>& matches, const vector<vector<bool>>& dictionary, size_t best_match_discharge) {
-    int index = 0;
-    for (size_t i = 0; i < matches.size(); i++) {
-        for (int j = 0; j < dictionary.size(); j++) {
-            if (dictionary[j] == matches[i]) {
-                index = j;
-                break;
-            }
-        }
-        cout << "[" << index << "] ";
-        for (size_t j = 0; j < matches[i].size(); j++) {
-            cout << matches[i][j];
-        }
-        cout << endl;
-    }
-
-    cout << "Max matches discharge: " << best_match_discharge << endl;
-}
-
-bool boolean_search(const vector<vector<bool>>& book, const string& argument, bool (BooleanFunctions::*bool_function)(const vector<bool>&)) {
-    for (size_t i = 0; i < book.size(); i++) {
-        const vector<bool>& word = book[i];
-        if ((boolean_functions.*bool_function)(word)) {
-            cout << "[" << i << "] ";
-            for (bool bit : word) {
-                cout << bit;
-            }
-            cout << endl;
-        }
-    }
-
-    return true;
-}
-
-
-class BooleanFunctions {
-public:
-    bool and_function(const vector<bool>& word) {
-        return word[0] && word[1] && word[2];
-    }
-
-    bool or_function(const vector<bool>& word) {
-        return word[0] || word[1] || word[2];
-    }
-
-    bool xor_function(const vector<bool>& word) {
-        return (word[0] && !word[1] && !word[2]) || (!word[0] && word[1] && !word[2]) || (!word[0] && !word[1] && word[2]);
-    }
 };
 
-BooleanFunctions boolean_functions;
+std::vector<std::vector<bool>> find_best_matches(const std::vector<std::vector<bool>>& book, const std::vector<bool>& word, int* best_match_discharge) {
+    std::vector<std::vector<bool>> best_matches;
+    *best_match_discharge = 0;
 
+    for (const std::vector<bool>& bookWord : book) {
+        std::cout << "Word to compare: ";
+        print_word(bookWord);
+        std::cout << std::endl;
 
+        if (word != bookWord) {
+            int current_best_match_discharge = 0;
+            auto mismatch = std::mismatch(word.begin(), word.end(), bookWord.begin());
+            current_best_match_discharge = std::distance(word.begin(), mismatch.first);
 
+            std::cout << "current_best_match_discharge = " << current_best_match_discharge << "  ";
+            if (current_best_match_discharge > *best_match_discharge) {
+                *best_match_discharge = current_best_match_discharge;
+                best_matches.clear();
+                best_matches.push_back(bookWord);
+            }
+            else if (current_best_match_discharge == *best_match_discharge) {
+                best_matches.push_back(bookWord);
+            }
+            std::cout << "best_match_discharge = " << *best_match_discharge << "  ";
+            std::cout << std::endl;
+        }
+    }
 
-class Dictionary {
+    return best_match;
+};
+
+class Functions {
 public:
-    Dictionary() {
+    std::function<bool(const std::vector<bool>&)> and = [](const std::vector<bool>& word) {
+        return word[0] && word[1] && word[2];
+    };
+
+    std::function<bool(const std::vector<bool>&)> or = [](const std::vector<bool>& word) {
+        return word[0] || word[1] || word[2];
+    };
+
+    std::function<bool(const std::vector<bool>&)> xor = [](const std::vector<bool>& word) {
+        return (word[0] && !word[1] && !word[2]) || (!word[0] && word[1] && !word[2]) || (!word[0] && !word[1] && word[2]);
+    };
+};
+
+
+class Dict {
+public:
+    Dict() {
         setlocale(LC_ALL, "Russian");
         words = get_random();
     }
 
     void print() const {
-        for (size_t i = 0; i < words.size(); i++) {
-            cout << "[" << i << "] ";
-            print_word(words[i]);
-            cout << endl;
+        size_t i = 0;
+        for (const auto& word : words) {
+            std::cout << "[" << std::to_string(i) << "] ";
+            print_word(word);
+            std::cout << std::endl;
+            i++;
         }
     }
-
-    void search_by_match() const {
-        cout << endl << "Enter the number of the word to search for matches: ";
-        size_t matching;
-        cin >> matching;
-        int best_match_discharge;
-        vector<vector<bool>> matches = find_best_matches(words, words[matching], &best_match_discharge);
-        cout << "Selected word - [" << matching << "] ";
-        print_word(words[matching]);
-        cout << endl;
-        cout << "Max similar words: " << endl;
-        print_matches(matches, words, best_match_discharge);
-        cout << endl << "Enter the word to search: ";
-        string word;
-        cin >> word;
-        for (size_t i = 0; i < words.size(); i++) {
-            cout << "[" << i << "]";
-            Trigger compare = recursive_function(word, words[i], 0);
-            cout << compare.g << " " << compare.l << endl;
-        }
-    }
-
-    void search_by_boolean_function() const {
-        cout << endl << "Enter the word to search: ";
-        string word;
-        cin >> word;
-        int operation;
-        cout << "Select a Boolean operation to search:" << endl;
-        cout << "1. AND" << endl;
-        cout << "2. OR" << endl;
-        cout << "3. XOR" << endl;
-        cin >> operation;
-        bool (BooleanFunctions::*bool_function)(const vector<bool>&) = nullptr;
-        switch (operation) {
-            case 1:
-                bool_function = &BooleanFunctions::and_function;
-                break;
-            case 2:
-                bool_function = &BooleanFunctions::or_function;
-                break;
-            case 3:
-                bool_function = &BooleanFunctions::xor_function;
-                break;
-            default:
-                cout << "Invalid operation" << endl;
-                return;
-        }
-
-        boolean_search(words, word, bool_function);
-    }
-
-private:
-    vector<vector<bool>> words;
 };
 
-Dictionary dictionary;
+    void match_search() const {
+    std::cout << std::endl << "Enter the number of the word to search for matches: ";
+    size_t matching;
+    std::cin >> matching;
+    int best_match_discharge;
+    std::vector<std::vector<bool>> matches = find_match(words, words[matching], &best_match_discharge);
+    std::cout << "Selected word - [" << matching << "] ";
+    print_word(words[matching]);
+    std::cout << std::endl;
+    std::cout << "Max similar words: " << std::endl;
+    print_match(matches, words, best_match_discharge);
+    std::cout << std::endl << "Enter the word to search: ";
+    std::string word;
+    std::cin >> word;
+    for (size_t i = 0; i < words.size(); i++) {
+        std::cout << "[" << i << "]";
+        Trigger compare = recursive_function(word, words[i], 0);
+        std::cout << compare.g << " " << compare.l << std::endl;
+    }
+};
+
+    void boolean_search() const {
+    std::cout << std::endl << "Enter the word to search: ";
+    std::string word;
+    std::cin >> word;
+    int operation;
+    std::cout << "Select a Boolean operation to search:" << std::endl;
+    std::cout << "1. AND" << std::endl;
+    std::cout << "2. OR" << std::endl;
+    std::cout << "3. XOR" << std::endl;
+    std::cin >> operation;
+
+    std::function<bool(const std::vector<bool>&)> bool_function = nullptr;
+    switch (operation) {
+        case 1:
+            bool_function = [this](const std::vector<bool>& word) { return booleanFunctions.and_function(word); };
+            break;
+        case 2:
+            bool_function = [this](const std::vector<bool>& word) { return booleanFunctions.or_function(word); };
+            break;
+        case 3:
+            bool_function = [this](const std::vector<bool>& word) { return booleanFunctions.xor_function(word); };
+            break;
+        default:
+            std::cout << "Invalid operation" << std::endl;
+            return;
+    }
+
+    boolean_search(words, word, bool_function);
+};
+
+Dict dict;
 
 int main() {
+    Dictionary dictionary;
+
+    std::cout << "Choose an operation:" << std::endl;
+    std::cout << "1 - Match search" << std::endl;
+    std::cout << "2 - Boolean search" << std::endl;
+
     int choice;
-    cout << "Choose an operation:" << "\n" << "1 - Search by match " << "\n" << "2 - Search based on Boolean function" << endl;
-    cin >> choice;
+    std::cin >> choice;
+
+    std::function<void()> operation = nullptr;
     if (choice == 1) {
-        dictionary.search_by_match();
+        operation = [&dictionary]() { dictionary.match_search(); };
+    } else if (choice == 2) {
+        operation = [&dictionary]() { dictionary.boolean_search(); };
+    } else {
+        std::cout << "Invalid choice" << std::endl;
+        return 0;
     }
-    else if (choice == 2) {
-        dictionary.search_by_boolean_function();
-    }
-    else {
-        cout << "Invalid choice" << endl;
-    }
+
+    operation();
 
     return 0;
 }
